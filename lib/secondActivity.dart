80b6138c9f5ad54b54e6229610143e72aca64d56
Christian Y. Castillo Covarrubias <cyccov@gmail.com>
2021-07-18 23:03:35 -0500

    Initial commit

---
+++ lib/secondActivity.dart
@@ -0,0 +1,41 @@
+import 'package:dropdownfield/dropdownfield.dart';
+import 'package:flutter/material.dart';
+
+
+class MyAppSecond extends StatelessWidget {
+
+  final String titulo = '';
+  String tipo_insumo_id = "";
+  String dependencia_insumo_id = "";
+  List<String> tipoDeInsumo = ["Gastable", "Medicamento", "Papeleria", "Material didactico", "Otros"];
+  List<String> procedenciaInsumo = ["Planeacion", "Almacen estatal", "Direccion de primer nivel de at", "Juridico", "Patrimonio social", "Segundo nivel de Atencion"];
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Reporte Insumos de Valija SSH")
+      ),
+      body: new Center(
+        child: new SingleChildScrollView(
+          child: new Column(
+            children: <Widget>[
+              new Text(
+                "Tipo de insumo:"
+              ),
+              DropDownField(
+                onValueChanged: (dynamic value){
+                  tipo_insumo_id = value;
+                },
+                value: tipo_insumo_id,
+                hintText: 'Selecciona el tipo de insumo',
+                labelText: '-- Tipo de insumo --',
+                items: tipoDeInsumo
+              ),
+            ]
+          )
+        )
+      )
+
+    );   
+  }
+}
