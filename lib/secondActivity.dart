5390dd12617a7e459e6414ff86bafe3e8661cdba
Christian Y. Castillo Covarrubias <cyccov@gmail.com>
2021-07-19 19:20:11 -0500

    Update secondActivity.dart

--- lib/secondActivity.dart
+++ lib/secondActivity.dart
@@ -1,14 +1,26 @@
 import 'package:dropdownfield/dropdownfield.dart';
 import 'package:flutter/material.dart';
 
-
 class MyAppSecond extends StatelessWidget {
 
   final String titulo = '';
   String tipo_insumo_id = "";
   String dependencia_insumo_id = "";
-  List<String> tipoDeInsumo = ["Gastable", "Medicamento", "Papeleria", "Material didactico", "Otros"];
-  List<String> procedenciaInsumo = ["Planeacion", "Almacen estatal", "Direccion de primer nivel de at", "Juridico", "Patrimonio social", "Segundo nivel de Atencion"];
+  List<String> tipoDeInsumo = [
+    "Material Medico Gastable", 
+    "Medicamento", 
+    "Papeleria", 
+    "Material didactico", 
+    "Otros"
+    ];
+  List<String> procedenciaInsumo = ["Planeacion", 
+  "Almacen Estatal SSH", 
+  "Direccion de Primer Nivel de Atención", 
+  "Jurídico", 
+  "Patrimonio social", 
+  "Segundo Nivel de Atencion",
+  "Hospital Niño DIF",
+  "Almacen Subsecretaria de Finanzas"];
   @override
   Widget build(BuildContext context){
     return Scaffold(
@@ -41,11 +53,29 @@ class MyAppSecond extends StatelessWidget {
                 labelText: "-- Dependencia --",
                 items: procedenciaInsumo
               ),
+              new Text("Descripcion del insumo: "),
+              new TextField(
+                //obscureText: true,
+                //controller: _multi
+                maxLines: 5,
+                decoration: new InputDecoration(
+                  //border: new OutlineInputBorder(),
+                  labelText: "Descripcion del insumo",
+                ),
+              ),
+              new Text("Cantidad recibida: "),
+              new TextField(
+                maxLength: 4,
+                expands: false,
+                decoration: new InputDecoration(
+                  labelText: "Cantidad recibida"
+                )
+              )
             ]
           )
         )
       )
-
+    
     );   
   }
 }
